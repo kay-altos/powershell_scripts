01698b249b2398b9ff963d0ea391aba2526866a9
kay-altos <greenwich32@yandex.ru>
2018-11-02 11:25:44 +0300

    add dhcpServer var

--- find-dhcpClient.ps1
+++ find-dhcpClient.ps1
@@ -2,22 +2,22 @@ Import-Module DhcpServer
 <#
     CliantId format: XX-YY-ZZ-VV-FF-LL
 #>
-
+$dhcpServer = ""
 $ClientId = "00-21-91-80-d4-74"
 #$IPAddress = "172.18.112.21"
 
 if(-not ([string]::IsNullOrEmpty($ClientId))){
-    $Scops = $(Get-DhcpServerv4Scope -ComputerName "dvorec-dc3.dvorec.net").ScopeId
+    $Scops = $(Get-DhcpServerv4Scope -ComputerName $dhcpServer).ScopeId
     ForEach ($Scop in $Scops) 
     {
-	    Get-DhcpServerv4Lease -ComputerName "dvorec-dc3.dvorec.net" -ScopeId $scop.IPAddressToString -ClientId $ClientId -ErrorVariable err -ErrorAction SilentlyContinue
+	    Get-DhcpServerv4Lease -ComputerName $dhcpServer -ScopeId $scop.IPAddressToString -ClientId $ClientId -ErrorVariable err -ErrorAction SilentlyContinue
        
     }
 
      Remove-Variable -Name ClientId
 } elseif (-not ([string]::IsNullOrEmpty($IPAddress))){
 
-    Get-DhcpServerv4Lease -ComputerName "dvorec-dc3.dvorec.net" -IPAddress $IPAddress -ErrorVariable err -ErrorAction SilentlyContinue
+    Get-DhcpServerv4Lease -ComputerName $dhcpServer -IPAddress $IPAddress -ErrorVariable err -ErrorAction SilentlyContinue
 
     Remove-Variable -Name IPAddress
 }
