52eff40cc1b3b63299dc5a60c11d619bf97d4f71
kay-altos <greenwich32@yandex.ru>
2018-11-13 15:02:51 +0300

    Param

--- cusctom-exten-script1.ps1
+++ cusctom-exten-script1.ps1
@@ -1,15 +1,13 @@
-Start-Transcript
 <#
     .SYNOPSIS
         1. Office16 Activation
 #>
 
-
-param (
+Param (
     [string]$nop
 )
 
-
+Start-Transcript
 Invoke-Command -ScriptBlock {cscript //B "C:\Program Files\Microsoft Office\Office16\ospp.vbs" /act}
 
 Stop-Transcript
