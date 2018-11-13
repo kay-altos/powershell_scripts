ea6101302f8f414842b0c3c250265c6b705deb02
kay-altos <greenwich32@yandex.ru>
2018-11-13 14:45:37 +0300

    param add

--- cusctom-exten-script1.ps1
+++ cusctom-exten-script1.ps1
@@ -1,5 +1,13 @@
 Start-Transcript
+<#
+    .SYNOPSIS
+        1. Office16 Activation
+#>
 
+
+Param (
+    [string]$nop
+)
 
 
 Invoke-Command -ScriptBlock {cscript //B "C:\Program Files\Microsoft Office\Office16\ospp.vbs" /act}
