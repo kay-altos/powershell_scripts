b9bb2d5712df2e040c666774941838e5b5626490
kay-altos <greenwich32@yandex.ru>
2018-12-13 14:37:04 +0300

    camp 2018 - 1

--- cusctom-exten-script1.ps1
+++ cusctom-exten-script1.ps1
@@ -10,4 +10,17 @@ Param (
 Start-Transcript
 Invoke-Command -ScriptBlock {cscript //B "C:\Program Files\Microsoft Office\Office16\ospp.vbs" /act}
 
+
+for($iu = 2; $iu -le 4; $iu++){
+
+    $PasswordU = "P@ssWord$iu"
+    $Secure_string = ConvertTo-SecureString $PasswordU -AsPlainText -Force
+    New-LocalUser "itcamp$iu" -Password $Secure_string  -FullName "Camp User $i" -Description "Camp User $i"
+    Add-LocalGroupMember -Group "Administrators" -Member "itcamp$iu"
+    Add-LocalGroupMember -Group "Remote Desktop Users" -Member "itcamp$iu"
+    echo "itcamp$iu"
+    echo $PasswordU
+}
+
+
 Stop-Transcript
